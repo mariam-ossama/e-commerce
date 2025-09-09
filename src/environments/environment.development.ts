c3efc27097cb332a7102d6445fe2c508e310f883
Mariam Mohamed Ossama <mariamosama772@gmail.com>
2025-09-09 21:18:41 +0300

    frontend url modification 2

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
     baseUrl: "https://ecommerce.routemisr.com/api/v1/",
-    frontEndUrl: "https://e-commerce.vercel.app"
+    frontEndUrl: "https://e-commerce-chi-dusky-19.vercel.app"
 };
